6e9280b535ffab9aaebbfe0fa894b3654c13b447
Antonella Aldao <antoaldao@gmail.com>
2025-11-02 22:16:36 -0300

    Implement CommandLineRunner in TareasApplication for task management initialization

--- src/main/java/com/utn/tareas/TareasApplication.java
+++ src/main/java/com/utn/tareas/TareasApplication.java
@@ -1,13 +1,45 @@
 package com.utn.tareas;
 
+import com.utn.tareas.model.Tarea;
+import com.utn.tareas.service.MensajeService;
+import com.utn.tareas.service.TareaService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TareasApplication {
+public class TareasApplication implements CommandLineRunner {
+
+	private final TareaService tareaService;
+	private final MensajeService mensajeService;
+
+	public TareasApplication(TareaService tareaService, MensajeService mensajeService) {
+		this.tareaService = tareaService;
+		this.mensajeService = mensajeService;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TareasApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) {
+		mensajeService.mostrarBienvenida();
+		tareaService.imprimirConfiguracion();
+		System.out.println("Tareas iniciales:");
+		tareaService.listarTodas().forEach(System.out::println);
+
+		tareaService.agregarTarea("Nueva tarea de prueba", Tarea.Prioridad.ALTA);
+		System.out.println("Tareas pendientes:");
+		tareaService.listarPendientes().forEach(System.out::println);
+
+		tareaService.marcarComoCompletada(1L);
+		System.out.println("Estadísticas:");
+		System.out.println(tareaService.obtenerEstadisticas());
+
+		System.out.println("Tareas completadas:");
+		tareaService.listarCompletadas().forEach(System.out::println);
+
+		mensajeService.mostrarDespedida();
+	}
 }
